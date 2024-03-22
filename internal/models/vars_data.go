2f5aac2b37ddc224339130b17d6a8da423433509
Pavel <pashamullin202@gmail.com>
2024-03-22 20:15:53 +0300

    update pj ,add base build for windows and add readme

--- internal/models/vars_data.go
+++ internal/models/vars_data.go
@@ -5,7 +5,7 @@ import "fyne.io/fyne/v2"
 const PreferenceCurrent = "current"
 const DbPath = "schedule.db"
 const FileKey = "file"
-const ProjectRepo = "https://github.com/pasha1coil/guiRSHUdata"
+const ProjectRepo = "https://github.com/pasha1coil/guiRSHUdata?tab=readme-ov-file#%D0%B3%D0%B5%D0%BD%D0%B5%D1%80%D0%B0%D1%82%D0%BE%D1%80-%D0%B5%D0%B6%D0%B5%D0%BC%D0%B5%D1%81%D1%8F%D1%87%D0%BD%D1%8B%D1%85-%D0%BE%D1%82%D1%87%D0%B5%D1%82%D0%BE%D0%B2"
 
 var TopWindow fyne.Window
 
